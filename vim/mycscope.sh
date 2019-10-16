cd6a935f8aab8faed76253f575138f5372db7a81
pkokp8 <xboxtwo@163.com>
2019-10-16 11:29:09 +0800

    update cscope script

--- vim/mycscope.sh
+++ vim/mycscope.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-# README: 
-# 1. pls modify /etc/crontab if rename this file.
-# 2. pls make sure the path parameter is correct.
-
-export NVM_DIR="/home/nzhou/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 SRCPATH=./
-ERRLOG=/home/nzhou/cronderr.log
+CSFILE=./
+ERRLOG=~/cronderr.log
 CURPWD=$PWD
-NPM=/home/nzhou/.nvm/versions/node/v4.8.7/bin/npm
+
+if [ ! -f $ERRLOG ]; then touch $ERRLOG; fi
 LOGFILESIZE=`ls $ERRLOG -l | awk -F\  '{print $5}'`
 LOGFILESIZE_KB=`expr $LOGFILESIZE / 1024`
 LOGFILESIZE_M=`expr $LOGFILESIZE_KB / 1024`
+TYPE="cpp"
 function output(){
     echo $1:$2 >> $ERRLOG
 }
@@ -23,47 +20,57 @@ if [ $LOGFILESIZE_M -ge 10 ];then
 fi
 
 if [ $1 ]; then
-    output "info" $1
     SRCPATH=$1
-elif [ -d ./src ] && [ -d ./node_modules ]; then
-    output "info" "./"
-fi
-
-output "info"  "=============start to update cscope===================" 
-date  >> $ERRLOG
-output "info"  "$SRCPATH" 
-
-if [ ! -d $SRCPATH -o ! -e $SRCPATH/node_modules ]; then 
-    output "error"  "$SRCPATH is not a directory or $SRCPATH/node_modules not exist" 
-    output "info"  "=============end to update cscope files===============" 
-    exit 100
-fi
-
-cd $SRCPATH
-CHANGELIST=`git st -s | grep -vE "^\?\?" | grep -v "gitignore"`
-if [ -z "$CHANGELIST" ]; then
-    output "info" "nothing changed"
-    git pull;
+    output "info" $1
 else
-    output "info"  "something changed" 
+    output "info" "SRCPATH ./"
 fi
 
-# rm ./node_modules -rf;
-$NPM install
-if [ $? -ne 0 ]; then
-    output "error"  "npm install failed" 
-    $NPM install >> $ERRLOG
+if [ $2 ]; then
+    CSFILE=$2;
+else
+    output "info" "CSFILE ./"
 fi
 
-if [ -f cscope.files ]; then 
+if [ $3 ]; then
+    TYPE=$3;
+fi
+
+output "info"  "=============start to update cscope==================="
+date  >> $ERRLOG
+
+if [ -f $CSFILE/cscope.files ]; then
+    cd $CSFILE
     rm cscope.files cscope.in.out cscope.po.out cscope.out
+    cd -
+    output "info"  "$CSFILE cscope files removed"
 fi
-find $SRCPATH/src -name "*.js" >> cscope.files
-find $SRCPATH/node_modules -name "*.js" >> cscope.files
-find $SRCPATH/test -name "*.js" >> cscope.files
+
+if [ $TYPE = "js" ]; then
+
+    if [ ! -d $SRCPATH -o ! -e $SRCPATH/package.json ]; then
+        output "error"  "$SRCPATH is not a directory or $SRCPATH/package.json not exist"
+        output "info"  "=============end to update $CSFILE/cscope.files==============="
+        exit 100
+    fi
+
+    cd $SRCPATH
+    # rm ./node_modules -rf;
+    find $SRCPATH/src -name "*.js" >> $CSFILE/cscope.files
+    find $SRCPATH/node_modules -name "*.js" >> $CSFILE/cscope.files
+    find $SRCPATH/test -name "*.js" >> $CSFILE/cscope.files
+elif [ $TYPE = "c" ]; then
+    find $SRCPATH -name "*.c" >> $CSFILE/cscope.files
+    find $SRCPATH -name "*.h" >> $CSFILE/cscope.files
+elif [ $TYPE = "cpp" ]; then
+    find $SRCPATH -name "*.cpp" >> $CSFILE/cscope.files
+    find $SRCPATH -name "*.hpp" >> $CSFILE/cscope.files
+    find $SRCPATH -name "*.c" >> $CSFILE/cscope.files
+    find $SRCPATH -name "*.h" >> $CSFILE/cscope.files
+fi
 cscope -Rbqk
 
-output "info"  "=============end to update cscope files===============" 
+output "info"  "=============end to update $CSFILE/cscope.files==============="
 output "info" ""
 
 cd $CURPWD
